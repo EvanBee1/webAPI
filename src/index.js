a54c4131e356ff6cd2b380971bfe3b1f738e53b8
GHOTHY <67308620+GHOTHY@users.noreply.github.com>
2024-04-26 16:14:39 +0800

    clean up js file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@ const express = require('express');
 const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
-const axios = require('axios');
-const apikey = '869744ce';
-const title = 'the Avengers';
 
 app.get("/", (req, res) => {
     fs.readFile('src/index.html', 'utf8', (err, data) => {
@@ -20,13 +17,4 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
-
-
-const querystr = `http://www.omdbapi.com/?t=${title}&apikey=${apikey}`
-
-axios.get(querystr).then ((response) => {
-    console.log(response.data.Title);
-    console.log(response.data.Year);
-    console.log(response.data.Director);
 });
